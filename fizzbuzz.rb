0832bedf67855a62c162335ca6d37049e585676b
Sarah Li <sarcl@hotmail.com>
2018-06-19 19:53:23 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizz_buzz(int)
+  if int % 15==0
+    puts "FizzBuzz"
+  elsif int % 5==0
+    puts"Buzz"
+  else int % 3==0
+    puts "Fizz"
+  end
+end
+fizz_buzz(30)
+fizz_buzz(60)
+fizz_buzz(6)
+
+
